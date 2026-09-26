fda560e2aa87d7bbf9b5d0aa14878cdd80665860
kylemiller629 <kmiller43@madisoncollege.edu>
2026-09-26 11:46:29 -0500

    added java docs in Run

--- src/main/java/edu/matc/entity/Run.java
+++ src/main/java/edu/matc/entity/Run.java
@@ -21,26 +21,50 @@ public class Run {
     @Column(name = "distance")
     private double distance;
 
+    /**
+     * Getter for ID
+     * @return id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Setter for ID
+     * @param id id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Getter for date of run
+     * @return date of run
+     */
     public LocalDate getRunDate() {
         return runDate;
     }
 
+    /**
+     * Setter for run date
+     * @param runDate date of run
+     */
     public void setRunDate(LocalDate runDate) {
         this.runDate = runDate;
     }
 
+    /**
+     *Getter for distance
+     * @return run distance
+     */
     public double getDistance() {
         return distance;
     }
 
+    /**
+     * Setter for distance
+     * @param distance run distance
+     */
     public void setDistance(double distance) {
         this.distance = distance;
     }
