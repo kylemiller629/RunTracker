bb9483773b0b628ac11707030db6b7ef45985a39
kylemiller629 <kmiller43@madisoncollege.edu>
2026-09-26 12:00:49 -0500

    added getById method in RunDao

--- src/main/java/edu/matc/persistence/RunDao.java
+++ src/main/java/edu/matc/persistence/RunDao.java
@@ -1,7 +1,9 @@
 package edu.matc.persistence;
 
+import edu.matc.entity.Run;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 /**
@@ -9,8 +11,22 @@ import org.hibernate.SessionFactory;
  * @author kmiller
  */
 public class RunDao {
+
     private final Logger logger = LogManager.getLogger(this.getClass());
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
 
+    /**
+     * Get run by id
+     * @param id run id
+     * @return run
+     */
+    public Run getById(int id) {
+        logger.info("getById" + id);
+        Session session = sessionFactory.openSession();
+
+        Run run = session.get(Run.class, id);
+        session.close();
+        return run;
+    }
 
 }
