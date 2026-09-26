cc9f147dd8bdeb32ab547ac9f87ebdd34fbb9d24
kylemiller629 <kmiller43@madisoncollege.edu>
2026-09-26 11:36:06 -0500

    started run class and added some annotations

--- src/main/java/edu/matc/entity/Run.java
+++ src/main/java/edu/matc/entity/Run.java
@@ -1,6 +1,24 @@
 package edu.matc.entity;
 
 import jakarta.persistence.*;
-import java.util.
+import java.time.LocalDate;
+
+/**
+ * Represents a run logged by a user
+ * @author kmiller
+ */
+@Entity
+@Table(name = "run")
 public class Run {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "run_date")
+    private LocalDate runDate;
+
+    @Column(name = "distance")
+    private double distance;
+
 }
